aa13861408887147825008c440270d96080eb581
unknown <Administrateur@139SE33-HXX04G2.ad.campus-eni.fr>
2021-08-25 15:05:27 +0200

    Liste de personnes avec Form ajout

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -3,11 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Personne;
+use App\Form\PersonneType;
 use App\Repository\PersonneRepository;
 
 use Doctrine\ORM\EntityManagerInterface;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -40,6 +42,32 @@ class PersonneController extends AbstractController
       $em->flush(); // on sauvegaer en base de données
       // redirection vers la route personne_liste
       return $this->redirectToRoute('personne_liste');
+
+
+    }
+
+     /**
+     * @Route("/add", name="personne_ajouter_2")
+     */
+    public function ajouter2(Request $request, EntityManagerInterface $em): Response
+    {
+        $personne = new Personne();
+        // je crée un objet formulaire associée à l entity $personne
+        $formPersonne = $this->createForm(PersonneType::class,$personne);
+        // je viens hydrater $personne en fonction $requet
+        // cad les donnees du formulaire
+        $formPersonne->handleRequest($request);
+        if ($formPersonne->isSubmitted())
+        {
+            $em->persist($personne);
+            $em->flush();
+            return $this->redirectToRoute('personne_liste');
+        }
+       
+      return $this->render('personne/ajouter.html.twig', [
+        'formPersonne' => $formPersonne->createView(),
+    ]);
+     
 
 
     }
